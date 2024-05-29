2920d201e49b2b56ca3867781606575cfe069d6d
Fredrik Viksten <fredrik.viksten@liu.se>
2024-05-29 16:59:34 +0200

    timer cloud

--- test.py
+++ test.py
@@ -19,8 +19,13 @@ class ScreenDisplayerNode(Node):
             self.get_logger().info('Services not available, waiting...')
         # image = Image.open("NEST-Background.png").convert('BGR')
         image = cv2.imread("NEST-Background.png") 
-        cload = cv2.imread("cloud1_small.png")
-        bee  = cv2.imread("yellowjacket-right-smallest.png")
+        cloud = cv2.imread("cloud1_small.png")
+        self.cloud_startx = self.bg_size[0]-100
+        self.cloud_x = self.cloud_startx
+        self.cloud_y = 400
+        bee  = cv2.imread("yellowjacket-left-smallest.png")
+        
+        self.timer = self.create_timer(0.2, self.move_cloud)  # Timer to call move_object every 0.1 seconds
 
         self.set_image(image)
         for i in range(0, 5760, 100):
@@ -46,6 +51,13 @@ class ScreenDisplayerNode(Node):
             image = np.zeros((1, 1, 3), dtype=np.uint8)
         request.image = self.bridge.cv2_to_imgmsg(image)        
         self.img_cli.call_async(request)
+
+    def move_cloud(self):
+        # Move the object to the right by 10 pixels
+        self.cloud_x -= 10
+        if self.cloud_x <= 0 #self.bg_size[0]:
+            self.cloud_x = self.cloud_startx  # Reset to the left edge if it goes off the screen
+        self.set_object(self.img_cloud, self.cloud_x, self.cloud_y, 'cloud')
 
 # Main function
 def main(args = None):
